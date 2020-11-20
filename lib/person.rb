a8c6bd6b57a173dd9f0b88ff5a711c34ea78fde8
Mnkith <munkithabid@gmail.com>
2020-11-20 00:34:33 -0600

    commit tree 0bf6cc0b26ab4b1712faba6c365c1a8e34719c4a

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,18 @@
 class Person
-  #your code here
+  def initialize(attributes)
+    attributes.each do |key, value|
+      self.class.attr_accessor(key)
+      self.send("#{key}=", value)
+    end
+  end
 end
+
+
+# puts Person.instance_methods.sort
+puts '======================================'
+p = Person.new({
+  :name => "Avi",
+  :birthday => "01/29/1984",
+  :hair_color => "brown"})
+puts p.name
+  puts Person.instance_methods.sort
